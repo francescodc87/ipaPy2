de726429b7949c58e6fa00bedd29ac9feedb5119
francescodc87 <francescodc87@gmail.com>
2022-09-13 14:55:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='ipaPy2',
     packages=find_packages(include=['ipaPy2']),
-    version='0.1.1',
+    version='0.1.2',
     description='Integrated Probabilistic Annotation (IPA) 2.0 - Python implementation ',
     author='Dr Francesco Del Carratore',
     author_email='francescodc87@gmail.com',

d94552253e718c8b7a8f6bdb612a6f40bcddb1f8
Francesco Del Carratore <francescodc87@gmail.com>
2023-03-08 17:51:59 +0000

    Update test_Compute_Bio.py

--- tests/test_Compute_Bio.py
+++ tests/test_Compute_Bio.py
@@ -5,18 +5,12 @@ def test_Compute_Bio():
     file = open('tests/test_Compute_Bio.pkl', 'rb')
     DB=pickle.load(file)
     annotations=pickle.load(file)
-    expected1 = pickle.load(file)
-    expected2 = pickle.load(file)
     file.close()
     
     Bio=ipa.Compute_Bio(DB,annotations,mode='reactions',ncores=1)
-    assert(Bio.equals(expected1))
+    assert(sorted(list(Bio.loc[0]))==['C00079', 'C02265'])
     Bio=ipa.Compute_Bio(DB,annotations,mode='reactions',ncores=2)
-    assert(Bio.equals(expected1))
-    Bio=ipa.Compute_Bio(DB,annotations,mode='connections',connections=['C4H2'],ncores=1)
-    assert(Bio.equals(expected2))
-    Bio=ipa.Compute_Bio(DB,annotations,mode='connections',connections=['C4H2'],ncores=2)
-    assert(Bio.equals(expected2))
-    
+    assert(sorted(list(Bio.loc[0]))==['C00079', 'C02265'])
+
     
         
